bdc87b900bd2ad9b86ccedba353dd8cde297307d
Adam Stachowicz <adam.stachowicz@fingo.info>
2021-04-12 12:33:24 +0200

    Package from private repo

---
+++ NuGetBug.WebApi/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestNETFrameworkClassLibrary;
+
+namespace NuGetBug.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetMessageFromNuGetPackage()
+        {
+            return Json(TestClass.PublicString);
+        }
+    }
+}
--- NuGetBug.WebApi/Controllers/WeatherForecastController.cs
+++ NuGetBug.WebApi/Controllers/WeatherForecastController.cs
@@ -10,7 +10,7 @@ namespace NuGetBug.WebApi.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        private static readonly string[] Summaries =
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
@@ -27,12 +27,12 @@ namespace NuGetBug.WebApi.Controllers
         {
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
         }
     }
 }
